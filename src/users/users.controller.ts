18a7f6882a261c9e11beb050a57466c3f9564c35
Medved Dima <kosolapiyd@gmail.com>
2025-11-17 20:18:52 +0200

    feat: implement UsersController and UsersService with CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  // GET /users
+  // GET /users/:id
+  // POST /users
+  // PUT /users/:id
+  // DELETE /users/:id
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.usersService.getUserById(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() user: { name: string; email: string }) {
+    return this.usersService.createUser(user);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() user: { name: string; email: string },
+  ) {
+    const updatedUser = {
+      id: Number(id),
+      ...user,
+    };
+    this.usersService.updateUser(updatedUser);
+    return updatedUser;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    this.usersService.deleteUser(Number(id));
+    return { message: 'User deleted successfully' };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,63 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      email: 'jane.smith@example.com',
+    },
+    {
+      id: 3,
+      name: 'Bob Johnson',
+      email: 'bob.johnson@example.com',
+    },
+    {
+      id: 4,
+      name: 'Alice Brown',
+      email: 'alice.brown@example.com',
+    },
+    {
+      id: 5,
+      name: 'David Lee',
+      email: 'david.lee@example.com',
+    },
+  ];
+
+  getUsers() {
+    return this.users;
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(user: { name: string; email: string }) {
+    const newUser = {
+      id: this.users.length + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateUser(updatedUser: { id: number; name: string; email: string }) {
+    const index = this.users.findIndex((user) => user.id === updatedUser.id);
+    if (index !== -1) {
+      this.users[index] = updatedUser;
+    }
+  }
+
+  deleteUser(id: number) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+    }
+  }
+}
